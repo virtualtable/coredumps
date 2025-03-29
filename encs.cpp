afc82aad8f983bb6d91ee34aa387fdffee77c576
virtualtable <149435163+virtualtable@users.noreply.github.com>
2025-03-29 21:35:05 +0200

    Update encs.cpp

--- encs.cpp
+++ encs.cpp
@@ -1,24 +1,25 @@
-//encs
-#define PROTO_MEMBER1_ENC VMValue4
-#define PROTO_MEMBER2_ENC VMValue1
-#define PROTO_DEBUGISN_ENC VMValue2
-#define PROTO_TYPEINFO_ENC VMValue3
-#define PROTO_DEBUGNAME_ENC VMValue4
+//coredumps.xyz
+#pragma once
 
-#define LSTATE_STACKSIZE_ENC VMValue2
-#define LSTATE_GLOBAL_ENC VMValue2
+#define PROTO_MEMBER1_ENC VMValue3 
+#define PROTO_MEMBER2_ENC VMValue2 
+#define PROTO_DEBUGISN_ENC VMValue4 
+#define PROTO_TYPEINFO_ENC VMValue1 
+#define PROTO_DEBUGNAME_ENC VMValue3 
 
-#define CLOSURE_FUNC_ENC VMValue4
-#define CLOSURE_CONT_ENC VMValue2
-#define CLOSURE_DEBUGNAME_ENC VMValue1
+#define LSTATE_STACKSIZE_ENC VMValue4 
+#define LSTATE_GLOBAL_ENC VMValue4 
 
-#define TABLE_MEMBER_ENC VMValue4
-#define TABLE_META_ENC VMValue4
+#define CLOSURE_FUNC_ENC VMValue3 
+#define CLOSURE_CONT_ENC VMValue4 
+#define CLOSURE_DEBUGNAME_ENC VMValue2 
 
-#define UDATA_META_ENC VMValue3
+#define TABLE_MEMBER_ENC VMValue3 
+#define TABLE_META_ENC VMValue3 
+#define UDATA_META_ENC VMValue1 
 
-#define TSTRING_HASH_ENC VMValue1
-#define TSTRING_LEN_ENC VMValue3
+#define TSTRING_HASH_ENC VMValue2 
+#define TSTRING_LEN_ENC VMValue1 
 
-#define GSTATE_TTNAME_ENC VMValue3
-#define GSTATE_TMNAME_ENC VMValue3
+#define GSTATE_TTNAME_ENC VMValue1 
+#define GSTATE_TMNAME_ENC VMValue1 
