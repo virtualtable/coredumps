93a0ee43bd425e0c72a8740fb8f15d2fffced83d
virtualtable <149435163+virtualtable@users.noreply.github.com>
2025-05-05 22:42:38 +0300

    Update shuffles.cpp

--- shuffles.cpp
+++ shuffles.cpp
@@ -1,14 +1,12 @@
 //coredumps.xyz
-//version-1e91b4133e334c9c
+//version-c3c1514fd260482e
 #pragma once
 
-#define LUAU_COMMA_SEP ,
-#define LUAU_SEMICOLON_SEP ;
-#define LUAU_SHUFFLE3(s, a1, a2, a3) a3 s a2 s a1
-#define LUAU_SHUFFLE4(s, a1, a2, a3, a4) a2 s a1 s a4 s a3
-#define LUAU_SHUFFLE5(s, a1, a2, a3, a4, a5) a4 s a3 s a2 s a5 s a1
-#define LUAU_SHUFFLE6(s, a1, a2, a3, a4, a5, a6) a2 s a6 s a1 s a5 s a4 s a3
-#define LUAU_SHUFFLE7(s, a1, a2, a3, a4, a5, a6, a7) a4 s a7 s a2 s a3 s a1 s a6 s a5
-#define LUAU_SHUFFLE8(s, a1, a2, a3, a4, a5, a6, a7, a8) a8 s a2 s a7 s a1 s a4 s a6 s a5 s a3
-#define LUAU_SHUFFLE9(s, a1, a2, a3, a4, a5, a6, a7, a8, a9) a5 s a3 s a4 s a7 s a8 s a9 s a2 s a6 s a1
+#define LUAU_SHUFFLE3(sep, a1, a2, a3) a2 sep a1 sep a3
+#define LUAU_SHUFFLE4(sep, a1, a2, a3, a4) a2 sep a1 sep a4 sep a3
+#define LUAU_SHUFFLE5(sep, a1, a2, a3, a4, a5) a2 sep a4 sep a1 sep a3 sep a5
+#define LUAU_SHUFFLE6(sep, a1, a2, a3, a4, a5, a6) a6 sep a5 sep a1 sep a2 sep a3 sep a4
+#define LUAU_SHUFFLE7(sep, a1, a2, a3, a4, a5, a6, a7) a7 sep a2 sep a6 sep a3 sep a5 sep a4 sep a1
+#define LUAU_SHUFFLE8(sep, a1, a2, a3, a4, a5, a6, a7, a8) a6 sep a2 sep a1 sep a7 sep a3 sep a8 sep a4 sep a5
+#define LUAU_SHUFFLE9(sep, a1, a2, a3, a4, a5, a6, a7, a8, a9) a2 sep a3 sep a4 sep a1 sep a9 sep a8 sep a5 sep a7 sep a6
 
