088186bc65f3d5d65d5466c062b8e6a1a11e46d0
Ducks <164558588+DucksEz@users.noreply.github.com>
2025-05-31 18:47:59 +0400

    Update encs.cpp
    updated encs

--- encs.cpp
+++ encs.cpp
@@ -1,29 +1,33 @@
 //coredumps.xyz
-//version-ff05edc617954c5b
+//version-ad3ee47cdc5e44f6
 #pragma once
 
-#define PROTO_MEMBER1_ENC VMValue0
-#define PROTO_MEMBER2_ENC VMValue1
-#define PROTO_DEBUGISN_ENC VMValue2
-#define PROTO_TYPEINFO_ENC VMValue3
-#define PROTO_DEBUGNAME_ENC VMValue4
 
-#define LSTATE_STACKSIZE_ENC VMValue2
-#define LSTATE_GLOBAL_ENC VMValue0
+#define PROTO_MEMBER1_ENC VMValue0 
+#define PROTO_MEMBER2_ENC VMValue2 
+#define PROTO_DEBUGISN_ENC vmvalue4 
+#define PROTO_TYPEINFO_ENC vmvalue1 
+#define PROTO_DEBUGNAME_ENC VMValue3 
 
-#define CLOSURE_FUNC_ENC VMValue0
-#define CLOSURE_CONT_ENC VMValue2
-#define CLOSURE_DEBUGNAME_ENC VMValue1
 
-#define TABLE_MEMBER_ENC VMValue0
-#define TABLE_META_ENC VMValue0
+#define LSTATE_STACKSIZE_ENC VMValue1 
+#define LSTATE_GLOBAL_ENC VMValue0 
 
-#define UDATA_META_ENC VMValue3
+#define CLOSURE_FUNC_ENC VMValue0 
+#define CLOSURE_CONT_ENC VMValue4 
+#define CLOSURE_DEBUGNAME_ENC VMValue2
 
-#define TSTRING_HASH_ENC VMValue1
-#define TSTRING_LEN_ENC VMValue0
 
-#define GSTATE_TTNAME_ENC VMValue0
-#define GSTATE_TMNAME_ENC VMValue0
+#define TABLE_META_ENC VMValue0 
+#define TABLE_MEMBER_ENC VMValue0  
+
+#define UDATA_META_ENC VMValue4 
+
+#define TSTRING_HASH_ENC VMValue3 
+#define TSTRING_LEN_ENC VMvalue0 
+
+
+#define GSTATE_TTNAME_ENC VMValue0 
+#define GSTATE_TMNAME_ENC VMValue0 
 
 
